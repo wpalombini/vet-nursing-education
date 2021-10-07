c384c8ef75cff5c038bd3b7995d7e593ba834295
Walter Palombini <walterpalombini@gmail.com>
2021-10-07 18:08:35 +1000

    get and post http methods

--- functions/articles/index.ts
+++ functions/articles/index.ts
@@ -1,61 +1,77 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+if (process.env.AZURE_FUNCTIONS_ENVIRONMENT === 'Development') {
+    require("dotenv").config();
+}
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { CosmosClient } from '@azure/cosmos';
+
+const getDbContainer= async () => {
+    const client = new CosmosClient({ 
+        endpoint: process.env.AZURE_COSMOS_ENDPOINT,
+        key: process.env.AZURE_COSMOS_KEY
+    });
+
+    const db = await client.databases.createIfNotExists({ id: process.env.AZURE_COSMOS_DB_ID });
+
+    return db.database;
+}
 
 const getArticles = async (context: Context, req: HttpRequest) => {
-    return await new Promise<any>((resolve) => {
-        resolve([
-            {
-                id: 1,
-                title: 'article title 1',
-                content: 'article content 1'
-            },
-            {
-                id: 2,
-                title: 'article title 2',
-                content: 'article content 2'
+    const db = await getDbContainer();
+
+    const articles = await db.container('articles').items.readAll().fetchAll();
+    
+    const data = await new Promise<any>((resolve) => {
+        resolve(articles.resources.map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+                content: item.content,
+                createdAt: item.createdAt,
             }
-        ])
+        }));
     })
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: data
+    };
 }
 
 const createArticle = async (context: Context, req: HttpRequest) => {
-    return await new Promise<any>((resolve) => {
-        resolve([
-            {
-                id: 1,
-                title: 'article title 1',
-                content: 'article content 1'
-            },
-            {
-                id: 2,
-                title: 'article title 2',
-                content: 'article content 2'
-            }
-        ])
-    })
+    const db = await getDbContainer();
+    
+    const article: any = {
+        id: new Date().getTime().toString(),
+        createdAt: new Date()
+    };
+
+    article.title = req.body.title;
+    article.content = req.body.content;
+
+    await db.container('articles').items.create(article);
+
+    context.res = {
+        status: 201,
+        body: article
+    };
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('Start article function request.');
-
-    let data;
+    context.log('Start article function request.', process.env);
 
     switch (req.method) {
         case "GET":
-            data = await getArticles(context, req);
+            await getArticles(context, req);
             break;
         case "POST":
-            data = await createArticle(context, req);
+            await createArticle(context, req);
             break;
     
         default:
             break;
     }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: data
-    };
-    context.log('End article function request.', { data });
+    context.log('End article function request.');
 };
 
 export default httpTrigger;
