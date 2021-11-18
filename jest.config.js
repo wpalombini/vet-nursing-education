8c23af56b54962b7bdbd73595f7824aee8a365e7
Walter Palombini <walterpalombini@gmail.com>
2021-11-18 21:21:13 +1000

    Automation tests setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+  },
+};
