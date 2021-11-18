82cd15e51061468058114214e625bb3b5dcaa547
Walter Palombini <walterpalombini@gmail.com>
2021-11-18 23:02:55 +1000

    Show loading progress bar on navigation

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,15 +1,32 @@
-import { forwardRef, ReactChild, useContext } from 'react';
+import { forwardRef, ReactChild, useContext, useEffect } from 'react';
 import { Container, LinearProgress, Snackbar } from '@mui/material';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
 import NavBar from './NavBar';
 import { UXContext } from '../../providers/UXProvider';
+import { useRouter } from 'next/router';
 
 interface ILayoutProps {
   children: ReactChild;
 }
 
 const Layout: (props: ILayoutProps) => JSX.Element = (props: ILayoutProps) => {
-  const { isLoading, notification, setNotification } = useContext(UXContext);
+  const { isLoading, setIsLoading, notification, setNotification } = useContext(UXContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      setIsLoading(true);
+    });
+    router.events.on('routeChangeComplete', () => {
+      setIsLoading(false);
+    });
+    return () => {
+      router.events.off('routeChangeComplete', () => {
+        console.log('stopped');
+      });
+    };
+  }, [router.events]);
 
   const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
